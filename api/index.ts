9079e015c1f2d4bd97a89ab24d598f6538ccb826
ahmed12345877 <q2w4115@gmail.com>
2026-05-16 03:12:12 +0300

    fix: complete serverless function export with app return

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,6 @@
-import app from '../server/_core/index';
-export default app;
+import { startServer } from '../server/_core/index';
+
+export default async function handler(req: any, res: any) {
+    const app = await startServer();
+    return app(req, res);
+}
