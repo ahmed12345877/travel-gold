e82a299239cb0569ea7a62e5c39b2daee5771b36
vanir-get <90734628+ahmed12345877@users.noreply.github.com>
2026-05-16 06:49:01 +0300

    Update database credentials in drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
     schema: './drizzle/schema.ts',
     dialect: 'mysql',
     dbCredentials: {
-        url: "mysql://3j1tVZMoLoquFZv.root:8vLIP5QMIrgjFDbk@gateway01.eu-central-1.prod.aws.tidbcloud.com:4000/test?ssl=%7B%22rejectUnauthorized%22:true%7D"
+        url: "mysql://3j1tVZMoLoquFZv.root:DniIuCWpj5QCbxY1@gateway01.eu-central-1.prod.aws.tidbcloud.com:4000/test?ssl=%7B%22rejectUnauthorized%22:true%7D"
     },
 });
