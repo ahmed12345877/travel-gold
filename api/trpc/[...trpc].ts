e1e1940156c1954c0f8bfc1416d652e6b99cb7af
vanir-get <90734628+ahmed12345877@users.noreply.github.com>
2026-05-31 23:29:51 +0300

    Update [...trpc].ts

--- api/trpc/[...trpc].ts
+++ api/trpc/[...trpc].ts
@@ -1,7 +1,9 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { nodeHTTPRequestHandler } from "@trpc/server/adapters/node-http";
-import { appRouter } from "../../server/routers";
-import { createContext } from "../../server/_core/context";
+// تحديث المسارات بإضافة /index بشكل صريح لمنع خطأ ERR_UNSUPPORTED_DIR_IMPORT
+import { appRouter } from "../../../server/routers/index";
+import { createContext } from "../../server/_core/context/index";
+
 
 // Cookie options type (inline to avoid express dependency issues)
 interface CookieOpts {
