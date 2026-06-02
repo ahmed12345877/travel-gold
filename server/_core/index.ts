8bd2c2aa3f4b0c471f098080e815a9fbc6b69dde
vanir-get <90734628+ahmed12345877@users.noreply.github.com>
2026-06-02 05:02:56 +0300

    Update index.ts

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -42,8 +42,6 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: "50mb" }));
-
-  app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   registerStorageProxy(app);
   registerDownloadProxy(app);
