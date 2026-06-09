cc97adea40c942588adba91e0d04830ea9ff7377
vanir-get <90734628+ahmed12345877@users.noreply.github.com>
2026-06-09 03:48:05 +0300

    Update [...trpc].ts

--- api/trpc/[...trpc].ts
+++ api/trpc/[...trpc].ts
@@ -1,5 +1,5 @@
 
-import type { IncomingMessage, ServerResponse } from "http";
+import type { IncomingMessage, ServerResponse } from "node:http";
 import { nodeHTTPRequestHandler } from "@trpc/server/adapters/node-http";
 // ✅ الصحيح: تحديد الملف والامتداد بدقة
 import { appRouter } from '../../server/routers/index.js';
