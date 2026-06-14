3e86792885e7ce87c9d62cfd90d9309661a16f79
v0agent <it+v0agent@vercel.com>
2026-06-14 17:13:25 +0000

    rename and update module imports in arc-D7FP7Vr-.js and architectureDiagram-3BPJPVTR-inkbmbkm.js
    Co-authored-by: vanir-get <90734628+ahmed12345877@users.noreply.github.com>

---
+++ dist/public/assets/chunk-QZHKN3VN-CkybYlnP.js
@@ -0,0 +1,1 @@
+import{a as s}from"./mermaid.core-I8IRU_GM.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
